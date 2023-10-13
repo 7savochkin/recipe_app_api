8fc4ec8d193096a2241693bf8b4c024f396b49a5
Oleg <savochkinoleg17@gmail.com>
2023-10-13 17:29:44 +0300

    build: create api filtering for tags and ingridients and image for recipe

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -29,3 +31,8 @@ urlpatterns = [
     path('api/v1/user/', include('user.urls')),
     path('api/v1/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
